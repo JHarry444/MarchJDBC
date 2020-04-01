37cdc1be9409f7d3e1be32708726bd93666cd41b
Jordan <jordan.harrison@qa.com>
2020-04-01 16:23:20 +0100

    Added executable jar

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -4,13 +4,9 @@ import java.sql.SQLException;
 
 public class Runner {
 
-
 	public static void main(String[] args) throws SQLException {
 		DB db = new DB();
-//		db.readActors();
-//		db.createActors("Matt", "Berk");
-//		db.deleteActor(201);
-		db.updateActor("Matt", "BERK", 44);
+		db.readActors();
 	}
 
 }
