9e125f361b760e466b24c0e4075d87d5bdc797ef
Jordan <jordan.harrison@qa.com>
2020-04-01 15:28:20 +0100

    Removed whitespace

--- src/main/java/main/DBConfig.java
+++ src/main/java/main/DBConfig.java
@@ -2,14 +2,10 @@ package main;
 
 public class DBConfig {
 
-	
 	public static final String DB_URL = "jdbc:mysql://35.241.217.5/sakila";
 	public static final String USER = "root";
 	public static final String PASS = "pass";
-	
-	
-	
-	
+
 	private DBConfig() {
 	}
 
