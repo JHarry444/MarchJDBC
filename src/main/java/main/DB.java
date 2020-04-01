19bf24495d81b2aa7264e9e040a96332dbc6802c
Jordan <jordan.harrison@qa.com>
2020-04-01 15:58:21 +0100

    Added CRUD functionality

--- src/main/java/main/DB.java
+++ src/main/java/main/DB.java
@@ -20,13 +20,26 @@ public class DB {
 		conn.close();
 	}
 
+	public void createActors(String fName, String lName) throws SQLException {
+		stmt.executeUpdate(
+				"INSERT INTO actor (`first_name`, `last_name`)" + " VALUES ('" + fName + "', '" + lName + "')");
+	}
+
 	public void readActors() throws SQLException {
 		ResultSet rs = stmt.executeQuery("SELECT * FROM actor");
 		while (rs.next()) {
 			String name = rs.getString("first_name") + " " + rs.getString("last_name");
 			System.out.println(name);
 		}
+	}
 
+	public void updateActor(String fName, String lName, int id) throws SQLException {
+		stmt.executeUpdate(
+				"UPDATE actor SET first_name = '" + fName + "', last_name = '" + lName + "' WHERE actor_id = " + id);
+	}
+
+	public void deleteActor(int id) throws SQLException {
+		stmt.executeUpdate("DELETE FROM actor WHERE actor_id = " + id);
 	}
 
 }
--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -7,7 +7,10 @@ public class Runner {
 
 	public static void main(String[] args) throws SQLException {
 		DB db = new DB();
-		db.readActors();
+//		db.readActors();
+//		db.createActors("Matt", "Berk");
+//		db.deleteActor(201);
+		db.updateActor("Matt", "BERK", 44);
 	}
 
 }
